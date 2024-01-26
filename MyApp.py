f8ccecc58a24aef1df206345a864fa2570844806
htorquato <htorquato@gmail.com>
2024-01-25 21:29:56 -0300

    Add files via upload

--- MyApp.py
+++ MyApp.py
@@ -42,9 +42,9 @@
 
 # Importação das bibliotecas a serem usadas
 import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
-import numpy as np
+# import seaborn as sns
+# import matplotlib.pyplot as plt
+# import numpy as np
 import streamlit as st
 import plotly.express as px 
 
